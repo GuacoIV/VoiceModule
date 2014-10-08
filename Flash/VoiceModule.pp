4059534dda2601d57da07d264ed17e64bba86e7d
GuacoIV <edward.lynch@att.net>
2014-10-07 21:23:13 -0500

    Stabilize Project by following example closely

---
+++ Flash/VoiceModule.pp
@@ -0,0 +1,99 @@
+# FIXED
+
+VoiceModule.obj: ../VoiceModule.c
+VoiceModule.obj: c:/ti/ccsv6/tools/compiler/c2000_6.2.8/include/stdio.h
+VoiceModule.obj: c:/ti/ccsv6/tools/compiler/c2000_6.2.8/include/linkage.h
+VoiceModule.obj: c:/ti/ccsv6/tools/compiler/c2000_6.2.8/include/stdarg.h
+VoiceModule.obj: c:/ti/ccsv6/tools/compiler/c2000_6.2.8/include/file.h
+VoiceModule.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/DSP28x_Project.h
+VoiceModule.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/F2802x_Device.h
+VoiceModule.obj: c:/ti/ccsv6/tools/compiler/c2000_6.2.8/include/assert.h
+VoiceModule.obj: c:/ti/ccsv6/tools/compiler/c2000_6.2.8/include/stdarg.h
+VoiceModule.obj: c:/ti/ccsv6/tools/compiler/c2000_6.2.8/include/stdbool.h
+VoiceModule.obj: c:/ti/ccsv6/tools/compiler/c2000_6.2.8/include/yvals.h
+VoiceModule.obj: c:/ti/ccsv6/tools/compiler/c2000_6.2.8/include/stdarg.h
+VoiceModule.obj: c:/ti/ccsv6/tools/compiler/c2000_6.2.8/include/_lock.h
+VoiceModule.obj: c:/ti/ccsv6/tools/compiler/c2000_6.2.8/include/stddef.h
+VoiceModule.obj: c:/ti/ccsv6/tools/compiler/c2000_6.2.8/include/stdint.h
+VoiceModule.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_Adc.h
+VoiceModule.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_BootVars.h
+VoiceModule.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_DevEmu.h
+VoiceModule.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_Comp.h
+VoiceModule.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_CpuTimers.h
+VoiceModule.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_ECap.h
+VoiceModule.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_EPwm.h
+VoiceModule.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_Gpio.h
+VoiceModule.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_I2c.h
+VoiceModule.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_NmiIntrupt.h
+VoiceModule.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_PieCtrl.h
+VoiceModule.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_PieVect.h
+VoiceModule.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_Spi.h
+VoiceModule.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_Sci.h
+VoiceModule.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_SysCtrl.h
+VoiceModule.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_XIntrupt.h
+VoiceModule.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/F2802x_Examples.h
+VoiceModule.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/F2802x_GlobalPrototypes.h
+VoiceModule.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/F2802x_EPwm_defines.h
+VoiceModule.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/F2802x_I2c_defines.h
+VoiceModule.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/F2802x_DefaultISR.h
+VoiceModule.obj: ../ti_ascii.h
+VoiceModule.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/adc.h
+VoiceModule.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/cpu.h
+VoiceModule.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/clk.h
+VoiceModule.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/pwm.h
+VoiceModule.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/flash.h
+VoiceModule.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/gpio.h
+VoiceModule.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/pie.h
+VoiceModule.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/pll.h
+VoiceModule.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/sci.h
+VoiceModule.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/sci_io.h
+VoiceModule.obj: C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/wdog.h
+
+../VoiceModule.c: 
+c:/ti/ccsv6/tools/compiler/c2000_6.2.8/include/stdio.h: 
+c:/ti/ccsv6/tools/compiler/c2000_6.2.8/include/linkage.h: 
+c:/ti/ccsv6/tools/compiler/c2000_6.2.8/include/stdarg.h: 
+c:/ti/ccsv6/tools/compiler/c2000_6.2.8/include/file.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/DSP28x_Project.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/F2802x_Device.h: 
+c:/ti/ccsv6/tools/compiler/c2000_6.2.8/include/assert.h: 
+c:/ti/ccsv6/tools/compiler/c2000_6.2.8/include/stdarg.h: 
+c:/ti/ccsv6/tools/compiler/c2000_6.2.8/include/stdbool.h: 
+c:/ti/ccsv6/tools/compiler/c2000_6.2.8/include/yvals.h: 
+c:/ti/ccsv6/tools/compiler/c2000_6.2.8/include/stdarg.h: 
+c:/ti/ccsv6/tools/compiler/c2000_6.2.8/include/_lock.h: 
+c:/ti/ccsv6/tools/compiler/c2000_6.2.8/include/stddef.h: 
+c:/ti/ccsv6/tools/compiler/c2000_6.2.8/include/stdint.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_Adc.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_BootVars.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_DevEmu.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_Comp.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_CpuTimers.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_ECap.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_EPwm.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_Gpio.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_I2c.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_NmiIntrupt.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_PieCtrl.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_PieVect.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_Spi.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_Sci.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_SysCtrl.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/include/F2802x_XIntrupt.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/F2802x_Examples.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/F2802x_GlobalPrototypes.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/F2802x_EPwm_defines.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/F2802x_I2c_defines.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/F2802x_DefaultISR.h: 
+../ti_ascii.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/adc.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/cpu.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/clk.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/pwm.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/flash.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/gpio.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/pie.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/pll.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/sci.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/sci_io.h: 
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/include/wdog.h: 
---
+++ Flash/subdir_vars.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_headers/cmd/F2802x_Headers_nonBIOS.cmd 
+
+LIB_SRCS += \
+C:/ti/controlSUITE/development_kits/C2000_LaunchPad/f2802x_common/lib/driverlib.lib 
+
+C_SRCS += \
+../VoiceModule.c 
+
+OBJS += \
+./VoiceModule.obj 
+
+C_DEPS += \
+./VoiceModule.pp 
+
+C_DEPS__QUOTED += \
+"VoiceModule.pp" 
+
+OBJS__QUOTED += \
+"VoiceModule.obj" 
+
+C_SRCS__QUOTED += \
+"../VoiceModule.c" 
+
+
